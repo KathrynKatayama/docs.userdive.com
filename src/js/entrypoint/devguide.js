949c8a1a45891225d287d7f12ad3c0c9a87d302e
Keisuke Kan <9renpoto@gmail.com>
2016-01-06 15:52:04 +0900

    remove i18n

--- src/js/entrypoint/devguide.js
+++ src/js/entrypoint/devguide.js
@@ -1,20 +1,4 @@
 import 'js/base.js'
 import 'highlight.css'
-import i18nextJquery from 'jquery-i18next'
-import i18next from 'i18next'
-import $ from 'jquery'
-import options from 'js/i18next-options.js'
 import hljs from 'highlight.js'
 hljs.initHighlightingOnLoad()
-
-i18next.init({
-  lng: options.lang,
-  load: 'languageOnly',
-  fallbackLng: 'en',
-  backend: {
-    loadPath: 'locales/{{lng}}/{{ns}}.json'
-  }
-}, (t, err) => {
-  i18nextJquery.init(i18next, $, options.defaults)
-  // $('.translate').localize()
-})
--- src/js/entrypoint/top.js
+++ src/js/entrypoint/top.js
@@ -1,17 +1,1 @@
 import 'js/base.js'
-import i18nextJquery from 'jquery-i18next'
-import i18next from 'i18next'
-import $ from 'jquery'
-import options from 'js/i18next-options.js'
-
-i18next.init({
-  lng: options.lang,
-  load: 'languageOnly',
-  fallbackLng: 'en',
-  backend: {
-    loadPath: 'locales/{{lng}}/{{ns}}.json'
-  }
-}, (t, err) => {
-  i18nextJquery.init(i18next, $, options.defaults)
-  // $('.translate').localize()
-})
